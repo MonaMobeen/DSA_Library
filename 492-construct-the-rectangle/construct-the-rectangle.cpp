37267df5ae68a80a3fd4984ea1e50b42dec7dd26
Memoona Mobeen <101993714+MonaMobeen@users.noreply.github.com>
2024-05-27 15:07:44 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (56.79%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
   vector<int> constructRectangle(int area) {
-    int width = sqrt(area); // Start with the largest possible width (square root of area)
-
-    // Iterate down from the square root until a divisor is found
+    int width = sqrt(area);  
+ 
     while (area % width != 0) {
       width--;
     }
